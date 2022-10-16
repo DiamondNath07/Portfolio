ab7c89132ea43f378e1d78ae0a7e1c67858cd272
DiamondNath07 <diamondnathaniel2@gmail.com>
2022-10-16 23:28:54 +0100

    add scroll feature to menu bar

--- app.js
+++ app.js
@@ -13,3 +13,41 @@ CloseBtn.addEventListener("click", () => {
     navLinks.classList.remove("showNav");
   }
 });
+
+// scroll functuinality
+const scrollLinks = document.querySelectorAll(".scroll-links");
+
+const navbar = document.querySelector(".nav");
+
+const linksContainer = document.querySelector(".links-container");
+
+scrollLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+
+    const navHeight = navbar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+
+    let position = element.offsetTop;
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+
+    // close menu
+    linksContainer.style.height = 0;
+    linksContainer.style.width = 0;
+  });
+});
+
+ const linksHeight = links.getBoundingClientRect().height;
+ const containerHeight = linksContainer.getBoundingClientRect().height;
+ if (containerHeight === 0) {
+   linksContainer.style.height = `${linksHeight}px`;
+ } else {
+   linksContainer.style.height = 0;
+ }

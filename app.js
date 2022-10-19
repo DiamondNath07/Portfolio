5fe1e9bf330f20af04a399c239046e4076dadd9d
DiamondNath07 <diamondnathaniel2@gmail.com>
2022-10-19 16:14:25 +0100

    add dynamic change to cards

--- app.js
+++ app.js
@@ -34,6 +34,66 @@ scrollLinks.forEach((item) => {
   });
 });
 
+const projectList = [
+  {
+    id: 1,
+    modalTitle: 'Data Dashboard Healthcare',
+    links: ['html', 'css', 'javascript'],
+    projectImg: './d-images/card.png',
+    text: ' Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur est unde totam modi magni? Ea culpa ducimus ad repudiandae? Molestias quae laborum consequatur tenetur odio iste atque temporibus veniam nemo vero aut mollitia, quam non itaque dolor totam debitis, adipisci accusantium quasi sequi in officia corporis. Voluptatum repellendus quia aliquam delectus blanditiis voluptatibus eos voluptates rem facilis',
+    livecode: 'https://genuine-crostata-91ee9b.netlify.app/',
+    sourcecode: 'https://github.com/DiamondNath07/Microsoft-start.git',
+  },
+  {
+    id: 2,
+    modalTitle: 'Health Care Services',
+    links: ['java', 'react', 'javascript'],
+    projectImg:
+      'https://img.freepik.com/premium-vector/creative-eye-catching-modern-horizontal-landscape-restaurant-food-flyer-template-design-02_647347-61.jpg?w=740',
+    text: 'Lorem ipsum esse nam soluta maxime, recusandae id culpa animi, corporis magnam placeat nisi! Sunt dicta porro cupiditate totam odio vero dignissimos! Magni inventore ab porro incidunt, dolores reiciendis beatae! Nesciunt quo, aperiam unde eveniet error perspiciatis tenetur cum expedita itaque blanditiis voluptates, non nam possimus magnam architecto. Pariatur dolorem possimus hic natus totam ad dignissimos quo libero nulla fugit sapiente consectetur sequi est officia quod, veritatis obcaecati non odit fuga sint! Possimus quasi quos laboriosam amet, fuga, consectetur facere et suscipit',
+    livecode: 'https://genuine-crostata-91ee9b.netlify.app/',
+    sourcecode: 'https://github.com/DiamondNath07/Microsoft-start.git',
+  },
+  {
+    id: 3,
+    modalTitle: 'Website Protfolio',
+    links: ['MongoDB', 'python', 'Nodejs'],
+    projectImg: './d-images/card.png',
+    text: 'Lorem ipsum Illo quaerat nostrum, quae atque dicta debitis quod voluptas laboriosam repudiandae sapiente velit consequuntur ratione provident sunt quibusdam sit earum, nisi id laborum accusantium quas sequi est? Cupiditate voluptatem omnis ab neque tempora esse animi ipsa recusandae eum. Ratione molestias qui impedit minus dolorum rem provident ',
+    livecode: 'https://genuine-crostata-91ee9b.netlify.app/',
+    sourcecode: 'https://github.com/DiamondNath07/Microsoft-start.git',
+  },
+  {
+    id: 4,
+    modalTitle: 'Profesional Art Printing Data More',
+    links: ['Angular', 'RubyOnRails', 'javascript'],
+    projectImg:
+      'https://img.freepik.com/premium-vector/creative-eye-catching-modern-horizontal-landscape-restaurant-food-flyer-template-design-02_647347-61.jpg?w=740',
+    text: 'Lorem ipsum debitis eaque magnam. Deleniti mollitia aspernatur excepturi, iste, ab molestias repellendus velit sed vero labore cumque. fugit eius, blanditiis pariatur, ea ipsum ex beatae cumque culpa iure nemo ut at delectus, consequuntur atque deserunt! Tenetur qui quasi, quibusdam inventore eum ex voluptas neque culpa ipsa incidunt nam tempora in itaque, dolores,',
+    livecode: 'https://genuine-crostata-91ee9b.netlify.app/',
+    sourcecode: 'https://github.com/DiamondNath07/Microsoft-start.git',
+  },
+  {
+    id: 5,
+    modalTitle: 'Health Care Services',
+    links: ['java', 'react', 'javascript'],
+    projectImg: './d-images/card2.png',
+    text: 'Lorem ipsum  accusamus repellendus facere! Eaque reprehenderit dicta iure blanditiis aspernatur? Magni tempore nemo alias quo accusantium ut, earum delectus eos deleniti doloremque id tenetur modi, qui, eligendi ab itaque ullam eaque ipsa impedit quam obcaecati? Accusantium minus perferendis mollitia cupiditate illum laborum eius unde quo explicabo tempore debitis expedita',
+    livecode: 'https://genuine-crostata-91ee9b.netlify.app/',
+    sourcecode: 'https://github.com/DiamondNath07/Microsoft-start.git',
+  },
+  {
+    id: 6,
+    modalTitle: 'Website Protfolio',
+    links: ['MongoDB', 'python', 'Nodejs'],
+    projectImg:
+      'https://img.freepik.com/premium-vector/creative-eye-catching-modern-horizontal-landscape-restaurant-food-flyer-template-design-02_647347-61.jpg?w=740',
+    text: 'Lorem ipsum dolor quidem nam quibusdam reprehenderit dolore ipsa dolorum, repudiandae non magni at suscipit recusandae corrupti sint! Corporis numquam impedit qui suscipit tempora. Sequi temporibus, nulla quis, molestias nihil accusantium quia ipsa laborum ut facere mollitia delectus quod libero',
+    livecode: 'https://genuine-crostata-91ee9b.netlify.app/',
+    sourcecode: 'https://github.com/DiamondNath07/Microsoft-start.git',
+  },
+];
+
 // cards object
 const kardsObj = [
   {
@@ -82,20 +142,16 @@ const kardsObj = [
 
 // modal effect
 
-
-
 // modal popup
 const modal = document.querySelector('.modal-overlay');
-const xIcon = document.querySelector('.x-icon');
 const projects = document.querySelector('#portfolio');
-
-
 
 kardsObj.map(({ id, mainText, description, techStack }) => {
   const element = document.createElement('div');
   const att = document.createAttribute('class');
   att.value = 'caad';
   element.setAttributeNode(att);
+  // element.classList.add('caad');
   element.innerHTML = `
  <div class="write-up">
         <p id="title">${mainText}</p>
@@ -115,13 +171,66 @@ kardsObj.map(({ id, mainText, description, techStack }) => {
   modalBtn.forEach((btn) => {
     btn.addEventListener('click', () => {
       modal.classList.add('open-modal');
+      mobile(id);
       // do something
-    });
-
-    xIcon.addEventListener('click', () => {
-      modal.classList.remove('open-modal');
     });
   });
 });
 
-// console.log(element);
+// destop version
+const modalbg = document.querySelector('.modal-overlay');
+// const xClose = document.querySelector('.x-icon');
+// const projectCards = document.querySelector('#portfolioB');
+
+function mobile(id) {
+  const mobileCard = projectList[id];
+
+  modal.innerHTML = `
+
+            <div class="modal-container">
+              <h5 class="modal-title">${mobileCard.modalTitle}</h5>
+              <img class="x-icon" src="./d-images/Icon - Cancel.png" alt="project-img">
+              <ul class="modal-links">
+                <li>${mobileCard.links[0]}</li>
+                <li>${mobileCard.links[1]}</li>
+                <li>${mobileCard.links[2]}</li>
+              </ul>
+              <div class="modal-wrapper">
+                <div class="project-imgContainer">
+                <img class="popimg" src="${mobileCard.projectImg}" alt="">
+              </div>
+
+                              
+              <p>${mobileCard.text}</p>
+              <div class="modal-btn">
+                 
+                  <span id="bur">
+                    <button class="burtin1">see live</button>
+                  <a class="livecode" href="${mobileCard.livecode}">
+                    <img class="export" src="./d-images/Icon - Export.png" alt="export">
+                  </a>
+                  </span>
+                  <span id="bur2">
+                    <button class="burtin2">see source</button>
+                 <a class="sourcecode" href="${mobileCard.sourcecode}"> <img class="github" src="./d-images/Group.png" alt="github"></a>
+                  </span>
+                
+              </div>
+              </div>
+
+            </div> `;
+  const xbtn = document.querySelector('.x-icon');
+  xbtn.addEventListener('click', () => {
+    modal.classList.remove('open-modal');
+    modal.innerHTML = '';
+  });
+}
+
+const modalBot = document.querySelectorAll('.modal-btn');
+modalBot.forEach((btn, i) => {
+  btn.addEventListener('click', () => {
+    console.log(i);
+    modalbg.classList.add('open-modalbg');
+    mobile(i);
+  });
+});
